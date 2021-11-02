41ccc6d2efcd8f77894a356fdd5ac02e0ed540d2
Hailey Thomas <haileyraebeauty@gmail.com>
2021-11-02 14:55:58 -0700

    create generate webpage function

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+// allows us to use node.js fs
+const fs = require('fs');
+// allows us to use generatePage function from page-template
+const generatePage = require('./src/page-template.js');
+
+// profile data array
 const profileDataArgs = process.argv.slice(2);
 
+// extract user command-line arguments and store them into distinct variables
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+// above is the same as below
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+/* EXAMPLES/CONCEPTS
 const printProfileData = profileDataArr => {
     // This...
     for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -13,3 +33,4 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData(profileDataArgs);
+*/
